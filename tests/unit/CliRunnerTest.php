a0f1d3642ad13d2e5c7640dd06205a8f94637892
hape <hapetlu@gmail.com>
2017-05-16 19:31:06 +0200

    test if minifier can read properties from a json file

--- tests/unit/CliRunnerTest.php
+++ tests/unit/CliRunnerTest.php
@@ -2,6 +2,7 @@
 
 use PHPUnit\Framework\TestCase; 
 use Phpmnfy\Runner as phpmnfy;
+use org\bovigo\vfs\vfsStream;
 
 final class CliRunnerTest extends TestCase
 {
@@ -16,5 +17,27 @@ EOT;
 		$minified = 'div.container {padding: 12px 6px;margin: 0;}';
 
 		$this->assertEquals($minified, (new phpmnfy)->compress($formatted));
+	}
+
+	public function testCliRunnerWithConfiguration()
+	{
+		$directory = [
+			'mnfy.json' => '{"files": ["assets/styles.css"]}',
+			'assets' => [
+				'style.css' => 'html {
+					padding: 12px 6px;
+					margin: 0;
+				}
+
+				.foo {
+					color: red;
+				}'
+			]
+		];
+
+		$filesystem = vfsStream::setup('root', 444, $directory);
+		$minifier = new Phpmnfy($filesystem->url() . '/mnfy.json');
+
+		$this->assertEquals(['assets/styles.css'], $minifier->files);
 	}
 }
