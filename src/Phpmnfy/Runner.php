62c304df7074370c6c5a2c75361b622c655fcff8
hape <hapetlu@gmail.com>
2017-05-16 19:31:53 +0200

    read configuration from a json file and store it in a property

--- src/Phpmnfy/Runner.php
+++ src/Phpmnfy/Runner.php
@@ -3,6 +3,23 @@ namespace Phpmnfy;
 
 class Runner 
 {
+	public $files; 
+
+	public function __construct($configurationFile = null)
+	{
+		if (is_null($configurationFile)) return; 
+
+		$configuration = json_decode(
+			file_get_contents($configurationFile),
+			true
+		);
+
+		if (!is_array($configuration)) return; 
+
+		if (array_key_exists('files', $configuration)) 
+			$this->files = $configuration['files'];
+	}
+
 	public function compress($input)
 	{
 		$output = Strip::newlines($input);
