dbf66bf8f77613b3e9f1a76d74047e3d60c24a49
Peter Lucht <1277738+plucht@users.noreply.github.com>
2018-04-07 23:57:43 +0200

    fix whitespaces

--- src/Phpmnfy/Strip.php
+++ src/Phpmnfy/Strip.php
@@ -4,19 +4,23 @@ namespace Phpmnfy;
 
 class Strip
 {
-	public static function newlines($input) {
-		return preg_replace('/(\r\n|\r|\n)/', '', $input);
-	}
+    public static function newlines($input)
+    {
+        return preg_replace('/(\r\n|\r|\n)/', '', $input);
+    }
 
-	public static function tabs($input) {
-		return preg_replace('/\t/', '', $input);
-	}
+    public static function tabs($input)
+    {
+        return preg_replace('/\t/', '', $input);
+    }
 
-	public static function inlineComments($input) {
-		return preg_replace('/\/\/.*\n/', '', $input);
-	}
+    public static function inlineComments($input)
+    {
+        return preg_replace('/\/\/.*\n/', '', $input);
+    }
 
-	public static function multilineComments($input) {
-		return preg_replace('|\/\*[.\s\w]*\*\/\s+|m', '', $input);
-	}
+    public static function multilineComments($input)
+    {
+        return preg_replace('|\/\*[.\s\w]*\*\/\s+|m', '', $input);
+    }
 }
--- tests/unit/StrippingCharactersTest.php
+++ tests/unit/StrippingCharactersTest.php
@@ -1,74 +1,76 @@
 <?php
 
-use PHPUnit\Framework\TestCase; 
+use PHPUnit\Framework\TestCase;
 use Phpmnfy\Strip;
 
 final class StrippingCharactersTest extends TestCase
 {
-	public function testStripNewlines()
-	{
-		$formatted = <<<'EOT'
+    public function testStripNewlines()
+    {
+        $formatted = <<<'EOT'
 div.container {
 	padding: 12px 6px;
 }
 EOT;
-		$minified = 'div.container {	padding: 12px 6px;}';
-		$this->assertEquals($minified, Strip::newlines($formatted));
-	}
+        $minified = 'div.container {	padding: 12px 6px;}';
+        $this->assertEquals($minified, Strip::newlines($formatted));
+    }
 
-	public function testStripTabs()
-	{
-		$formatted = <<<'EOT'
+    public function testStripTabs()
+    {
+        $formatted = <<<'EOT'
 div.container {
 	padding: 12px 6px;
 	margin-top: 8px;
 }
 EOT;
-		$minified = <<<'EOT'
+        $minified = <<<'EOT'
 div.container {
 padding: 12px 6px;
 margin-top: 8px;
 }
 EOT;
-		$this->assertEquals($minified, Strip::tabs($formatted));
-	}
+        $this->assertEquals($minified, Strip::tabs($formatted));
+    }
 
-	public function testStripTabsAndNewlines()
-	{
-		$formatted = <<<'EOT'
+    public function testStripTabsAndNewlines()
+    {
+        $formatted = <<<'EOT'
 div.container {
 	padding: 12px 6px;
 	margin-top: 8px;
 }
 EOT;
-		$minified = 'div.container {padding: 12px 6px;margin-top: 8px;}';
-		$this->assertEquals($minified, Strip::tabs(
-				Strip::newlines($formatted)
-			)
-		);
-	}
+        $minified = 'div.container {padding: 12px 6px;margin-top: 8px;}';
+        $this->assertEquals(
+            $minified,
+            Strip::tabs(
+                Strip::newlines($formatted)
+            )
+        );
+    }
 
-	public function testStripInlineComments()
-	{
-		$formatted = <<<EOT
+    public function testStripInlineComments()
+    {
+        $formatted = <<<EOT
 // “This License” refers to version 3 of the GNU General Public License.
 var props = {
 	margin: 0,
 	padding: 0
 };
 EOT;
-		$minified = <<<EOT
+        $minified = <<<EOT
 var props = {
 	margin: 0,
 	padding: 0
 };
 EOT;
-		$this->assertEquals($minified, Strip::inlineComments($formatted));
-	}
+        $this->assertEquals($minified, Strip::inlineComments($formatted));
+    }
 
-	public function testStripMultilineComments()
-	{
-		$formatted = <<<'EOT'
+    public function testStripMultilineComments()
+    {
+        $formatted = <<<'EOT'
 html {
 	/* margin */
 	margin: 0;
@@ -83,13 +85,13 @@ html {
 	/* really the end of comments */
 };
 EOT;
-		$minified = <<<EEE
+        $minified = <<<EEE
 html {
 	margin: 0;
 	padding: 0;
 	background: red;
 	};
 EEE;
-		$this->assertEquals($minified, Strip::multilineComments($formatted));
-	}
+        $this->assertEquals($minified, Strip::multilineComments($formatted));
+    }
 }
